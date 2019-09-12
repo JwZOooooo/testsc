22cf5f41b18b6ec0f0b3911d51d7e40c1b60a817
Jinwei Zhang <jinwei.zhang@pcitc.com>
2019-09-12 14:47:52 +0800

    testsc01项目新建

---
+++ src/main/java/com/sc/testsc/TestscApplication.java
@@ -0,0 +1,13 @@
+package com.sc.testsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sc/testsc/TestscApplicationTests.java
@@ -0,0 +1,16 @@
+package com.sc.testsc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestscApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
